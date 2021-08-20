53c55ef81de2bc928e2bc89381e8e043f23d12d9
Kimberly Salas <salask0104@gmail.com>
2021-08-20 14:40:09 -0700

    added nav bar with icon

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,41 @@
+//this will store all content
+import React from 'react'
+import {FaBars} from 'react-icons/fa'
+import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks } from './NavbarElements';
+
+const Navbar = () => {
+    return (
+      <>
+        <Nav>
+            <NavbarContainer>
+                <NavLogo to='/'>KS</NavLogo>
+                {/* Mobile Hamburger Buttom */}
+                <MobileIcon>
+                    <FaBars />
+                </MobileIcon>
+                {/* END Mobile Hamburger Buttom */}
+                
+                {/* Nav links */}
+                <NavMenu>
+                    <NavItem>
+                        <NavLinks to="about">About Me</NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to="portfolio">Portfolio</NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to="contact">Contact</NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to="about">Resume</NavLinks>
+                    </NavItem>
+                </NavMenu>
+                {/* END of Nav links */}
+
+            </NavbarContainer>
+        </Nav>
+      </>  
+    );
+}
+
+export default Navbar
